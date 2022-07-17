6e1b1ae45457000266485d6b42f61cf7dcc9f7a9
Danylo Tseselskyi <danylotseselskyi@gmail.com>
2022-07-17 13:58:03 +0300

    Person form

--- triangle/urls.py
+++ triangle/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from triangle.views import get_form
+from triangle.views import PersonIndexView, get_form, person_create_form, person_update_form
 
 app_name = 'triangle'
 urlpatterns = [
-    path('', get_form, name='index')
+    path('', get_form, name='index'),
+    path('person-list/', PersonIndexView.as_view(), name='person-list'),
+    path('person/', person_create_form, name='person-form'),
+    path('person/<int:pk>/', person_update_form, name='person-update-form'),
 ]
